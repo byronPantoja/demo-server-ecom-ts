e23710366a32d8802093f54e69b57d0978ca9bd1
“dbp” <byronpantoja@gmail.com>
2021-04-22 17:31:39 +0800

    creating firebase config file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_api_key,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
